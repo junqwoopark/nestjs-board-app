dac39e6018568a1e94e1287168aa6fc0b8aad628
Junkyu Park <junqwoopark@gmail.com>
2023-05-29 03:21:33 +0900

    study: signUp 기능 구현, user 데이터 유효성 검사, 비밀번호 암호화

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { User } from './user.entity';
 import { DataSource, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -11,5 +17,24 @@ export class UserRepository extends Repository<User> {
       baseRepository.manager,
       baseRepository.queryRunner,
     );
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
